fc823e6125ce495021006ef75743e9c2bfea4917
ChenTongGit <1329075259@qq.com>
2020-07-01 20:50:16 +0800

    add d

---
+++ boss-xtrain-bes/boss-xtrain-common/boss-xtrain-core/src/main/java/com/boss/train/bes/common/core/ApplicationMain.java
@@ -0,0 +1,15 @@
+package com.boss.train.bes.common.core;
+
+/**
+ * @author ChenTong
+ * @date 2020/7/1 20:40
+ * @copyright
+ * @modified
+ * @see
+ * @since
+ **/
+public class ApplicationMain {
+    public static void main(String[] args) {
+
+    }
+}
