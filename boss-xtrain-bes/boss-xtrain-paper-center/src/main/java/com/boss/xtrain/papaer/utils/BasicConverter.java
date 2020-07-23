af1bc753400e90d5db92433099ec33b9ff370446
zhangzhongyi <873190934@qq.com>
2020-07-24 00:04:09 +0800

    zzy修改

--- boss-xtrain-bes/boss-xtrain-paper-center/src/main/java/com/boss/xtrain/papaer/utils/BasicConverter.java
+++ boss-xtrain-bes/boss-xtrain-paper-center/src/main/java/com/boss/xtrain/papaer/utils/BasicConverter.java
@@ -7,13 +7,7 @@ import org.springframework.cglib.core.Converter;
 public class BasicConverter implements Converter {
     static final Byte RIGHT_SIGN = 1;
     static final Byte FALSE_SIGN = 2;
-    /**
-     * Description: value:源属性对象，target:目标属性对象，context:目标属性对象的set方法
-     * @author zjh
-     * @date 21:02 2020/4/1
-     * @param value, target, context
-     * @return java.lang.Object
-     */
+
     @Override
     public Object convert(Object value, Class target, Object context) {
         //如果源属性对象为Long，目标属性对象为String，则将Long转成String
--- boss-xtrain-bes/boss-xtrain-paper-center/src/main/java/com/boss/xtrain/papaer/utils/SomeParamChange.java
+++ boss-xtrain-bes/boss-xtrain-paper-center/src/main/java/com/boss/xtrain/papaer/utils/SomeParamChange.java
@@ -1,26 +1,5 @@
 package com.boss.xtrain.papaer.utils;
 
 public class SomeParamChange {
-    public static Long difficultyChange(String source){
-        if(source.equals("简单")){
-            return Long.valueOf(123231 );
-        }else if(source.equals("正常")){
-            return Long.valueOf(12345);
-        }else if(source.equals("复杂")){
-            return  Long.valueOf(31312);
-        }else {
-            return null;
-        }
-    }
-    public static Long categoryChange(String source){
-        if(source.equals("java")){
-            return Long.valueOf(123231);
-        }else if(source.equals("python")){
-            return Long.valueOf(12345);
-        }else if(source.equals("linux")){
-            return  Long.valueOf(31312);
-        }else {
-            return null;
-        }
-    }
+
 }
